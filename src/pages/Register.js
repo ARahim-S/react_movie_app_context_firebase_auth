161b9300384df3d209ef07de3b031c68d3aa6958
ARahim-S <arahim.s@yandex.com>
2023-04-13 22:20:23 +0300

    v5

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -9,13 +9,42 @@ const Register = () => {
       <div className="register-form">
         <h1 className="form-title display-3">Register</h1>
         <form id="register" className="register-form">
-          <label htmlFor="first-name">First Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="first-name"
-            placeholder="Enter your first name"
-          />
+          <div className="mb-3">
+            <label htmlFor="first-name">First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="first-name"
+              placeholder="Enter your first name"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="last-name"
+              placeholder="Enter your last name"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </div>
         </form>
       </div>
     </div>
