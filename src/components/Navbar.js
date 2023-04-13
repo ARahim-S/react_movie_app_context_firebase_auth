86c9c4e5fced00b98ba3e6d65daf93db59d4d611
ARahim-S <arahim.s@yandex.com>
2023-04-13 21:52:49 +0300

    navbar ended

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const currentUser = { displayName: "ARahim" };
   return (
     <div>
@@ -10,19 +11,31 @@ const Navbar = () => {
           <Link className="navbar-brand text-white" to={"/"}>
             <h4> Best Movie App</h4>
           </Link>
-          {currentUser ? (
-            <div className="d-flex text-white align-items-center">
-              <h5 className="mb-0 text-capitalize">
-                {currentUser?.displayName}
-              </h5>
-              <button className="ms-2 btn btn-outline-light">Logout</button>
-            </div>
-          ) : (
-            <div className="d-flex text-white align-items-center">
-              <button className="ms-2 btn btn-outline-light">Login</button>
-              <button className="ms-2 btn btn-outline-light">Register</button>
-            </div>
-          )}
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser?.displayName}
+                </h5>
+                <button className="ms-2 btn btn-outline-light">Logout</button>
+              </>
+            ) : (
+              <>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </button>
+              </>
+            )}
+          </div>
         </div>
       </nav>
     </div>
