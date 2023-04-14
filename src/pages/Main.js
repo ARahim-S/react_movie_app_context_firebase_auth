45fd5e4fccf30eec3950f20c7994483a54aaca14
ARahim-S <arahim.s@yandex.com>
2023-04-14 18:48:29 +0300

    api implemented

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.message));
+  };
+
   return <div>Main</div>;
 };
 
