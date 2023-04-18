44a3ffd4018e2c19ad596c75bd5bca9976704bc0
ARahim-S <arahim.s@yandex.com>
2023-04-18 13:15:30 +0300

    updated

---
+++ src/components/MovieCard.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { createUser } from "../auth/firebase";
+
+const IMG_API = "https://image.tmdb.org/t/p/w500";
+const defaultImage =
+  "https://source.unsplash.com/random/1920x1080/?wallpaper,landscape";
+
+const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
+  const { currentUser } = useContext(AuthContext);
+  return (
+    <div className="movie">
+      <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="" />
+      <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
+        <h5>{title}</h5>
+        {currentUser && (
+          <span
+            className={`tag ${
+              vote_average >= 8 ? "green" : vote_average >= 6 ? "orange" : "red"
+            }`}
+          >
+            {vote_average}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
