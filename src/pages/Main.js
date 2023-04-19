fea433957884fbf87a1430b2bdd097fc4e6c807a
ARahim-S <arahim.s@yandex.com>
2023-04-19 19:38:02 +0300

    updated 3

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/AuthContext";
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 console.log(API_KEY);
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     getMovies(FEATURED_API);
@@ -26,12 +29,36 @@ const Main = () => {
       );
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm && currentUser) {
+      getMovies(SEARCH_API + searchTerm);
+    } else if (!currentUser) {
+      toast.error("Please log in to search a movie");
+    } else {
+      toast.error("Please enter a text");
+    }
+  };
+
   return (
-    <div className="d-flex justify-content-center flex-wrap">
-      {movies?.map((movie) => (
-        <MovieCard key={movie.id} {...movie} />
-      ))}
-    </div>
+    <>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <div className="d-flex justify-content-center flex-wrap">
+        {movies?.map((movie) => (
+          <MovieCard key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>
   );
 };
 
