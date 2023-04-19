d3511141546bc1742670a7019b2b8a05542ae020
ARahim-S <arahim.s@yandex.com>
2023-04-19 23:41:44 +0300

    updated 5

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+  Outlet,
+} from "react-router-dom";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
 import Navbar from "../components/Navbar";
 import MovieDetail from "../pages/MovieDetail";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 const AppRouter = () => {
+  const currentUser = useContext(AuthContext);
+  const PrivateRouter = () => {
+    const location = useLocation();
+    if (!currentUser) {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return <Outlet />;
+  };
   return (
     <div>
       <Router>
@@ -14,7 +32,9 @@ const AppRouter = () => {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/details/:id" element={<MovieDetail />} />
+          <Route element={<PrivateRouter />}>
+            <Route path="/details/:id" element={<MovieDetail />} />
+          </Route>
         </Routes>
       </Router>
     </div>
